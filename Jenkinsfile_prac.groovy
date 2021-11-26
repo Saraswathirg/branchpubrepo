f14036588935e7a3d82c9fe1759c54544a73f4e1
saraswathirg <4saraswathi95@gmail.com>
2021-11-26 13:15:47 +0530

    added steps

---
+++ Jenkinsfile_prac.groovy
@@ -0,0 +1,33 @@
+//declarative pipeline
+pipeline{
+    agent any
+    parameters{
+        string(name:'BRANCH',defaultValue:'master',description:'enter the branch')
+    stages{
+        stage("clone the code"){
+            steps{
+                println "the code is cloned"
+                checkout([
+                    $class:'GitSCM',
+                    branches:[[name:'${BRANCH}']],
+                    userRemoteConfigs:[[url:'https://github.com/Saraswathirg/branchpubrepo.git']]
+                ])
+            }
+        }
+        stage("build the code"){
+            steps{
+                println "the code is built"
+                sh"""
+                ls -lart ./*
+                mvn clean package"""
+            }
+        }
+        stage("store to s3"){
+            steps{
+                println "the artifact stored to s3"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://alltime/${BRANCH}/${BUILD_NUMBER}/"
+            }
+        }
+        }
+    }
+}
