0597f5579db16217429c751106b62d1fe202ad6d
saraswathirg <4saraswathi95@gmail.com>
2021-12-07 10:35:21 +0530

    build steps

---
+++ Jenkins_build.groovy
@@ -0,0 +1,34 @@
+pipeline{
+    agent any
+    parameters{
+        string(name='BRANCH',defaultValue='master',description='enter the branch name')
+    }
+    stages{
+        stage("check updates in repo"){
+            steps{
+                println "here checks for updates"
+                checkout([
+                    $class:'GitSCM',
+                    branches:[[name='${BRANCH}']],
+                    userRemoteConfigs:[[url'https://github.com/Saraswathirg/branchpubrepo.git']]
+                ])
+            }
+        }
+        stage("build the code"){
+            steps{
+                println "the code is built"
+                sh"""
+                ls -lart./
+                mvn clean package"""
+            }
+        }
+        stage("the artifact is stored"){
+            steps{
+                println "the artifact is stored"
+                sh"
+                aws s3 cp target/hello-${BUILD}.war s3://alltime/${BRANCH}/${BUILD}/
+            }
+        }
+    }
+
+}
