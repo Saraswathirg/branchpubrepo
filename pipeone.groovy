becb3e1a96faa02899a127a8435b621b6d8c161c
saraswathirg <4saraswathi95@gmail.com>
2022-01-03 22:59:49 +0530

    added changes

--- pipeone.groovy
+++ pipeone.groovy
@@ -30,21 +30,20 @@ pipeline{
         stage("the code is copied to s3"){
             steps{
                 println "the code is stored to s3"
-                sh " aws s3 cp target/hello-${BUILD_NUM}.war s3://alltime/${BRANCH}/${BUILD_NUM}/"
+                sh "aws s3 cp target/hello-${BUILD_NUM}.war s3://alltime/${BRANCH}/${BUILD_NUM}/"
             }
         }
         stage("copy artifact"){
             steps{
                 println "the code is copied from s3"
-                sh"
-                aws s3 cp s3://alltime/${BRANCH}/${BUILD_NUM}/hello-${BUILD_NUM}.war ."
+                sh "aws s3 cp s3://alltime/${BRANCH}/${BUILD_NUM}/hello-${BUILD_NUM}.war ."
 
             }
         }
         stage(" transfer the code to deploy"){
             steps{
                 println "the artifact is copied"
-                sh " scp -o StrictHostKeyChecking=no /tmp/awsaws.pem hello-${BUILD_NUM}.war ec2-user@${SERVER_IP}:/var/lib/tomcat/webapps"
+                sh "scp -o StrictHostKeyChecking=no /tmp/awsaws.pem hello-${BUILD_NUM}.war ec2-user@${SERVER_IP}:/var/lib/tomcat/webapps"
             }
         }
 
